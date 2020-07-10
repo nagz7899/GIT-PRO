10bbbd60fb0952a1235582daf28d8fc05752070d
nagz7899 <61879294+nagz7899@users.noreply.github.com>
2020-07-10 19:48:27 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/haresh/tic_tac_toe_app/two_player.java
@@ -0,0 +1,19 @@
+package com.example.haresh.tic_tac_toe_app;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+
+public class two_player extends AppCompatActivity {
+
+    public static Activity act_2p;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        act_2p = this;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_player);
+    }
+}
